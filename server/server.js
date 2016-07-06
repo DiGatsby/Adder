903d24f0ac278def3d9a6388a0e556541c1500c2
Viltzu <viltzu2@gmail.com>
2016-07-07 02:04:15 +0300

    Server now checks for line intersections rather than just checking distance, turn point count limited based on time

--- server/server.js
+++ server/server.js
@@ -33,7 +33,8 @@ io.on('connection', function (socket) {
 		length: 0,
 		lineData: [{"x": rx, "y": ry}, {"x": rx, "y": ry}],
 		alive: true,
-		gap: 0
+		gap: 0,
+		lastAdd: Date.now()
 	}
 	
 	// Send player instance to the connected player as "y" (yourself)
@@ -44,10 +45,14 @@ io.on('connection', function (socket) {
 	socket.on('a', function(a) {
 		players[socket.id].a = a;
 		if (players[socket.id].gap < 0) {
+			if (Date.now() - players[socket.id].lastAdd > 40) {
 			io.sockets.emit('f', socket.id); // Tell everyone to Fix latest segment for this id (by appending new point to lineData)
 			
-			var lineData = players[socket.id].lineData;
-			players[socket.id].lineData.push(lineData[lineData.length - 1]);
+			
+				var lineData = players[socket.id].lineData;
+				players[socket.id].lineData.push(lineData[lineData.length - 1]);
+				players[socket.id].lastAdd = Date.now();
+			}
 		}
 	});
 
@@ -84,7 +89,7 @@ function updateLoop() {
 					io.sockets.emit('p', {"id": players[key].id, "ld": {"x": x, "y": y} });
 				}
 				
-				players[key].lineData[players[key].lineData.length - 1] = {"x": x + 2.5 * Math.sin(players[key].a), "y": y + 2.5 * Math.cos(players[key].a)};
+				players[key].lineData[players[key].lineData.length - 1] = {"x": x + 5 * Math.sin(players[key].a), "y": y + 5 * Math.cos(players[key].a)};
 				players[key].gap -= 1;
 				if (players[key].gap < 0) {
 					//players[key].lineData[players[key].lineData.length - 1].x += ;
@@ -105,35 +110,31 @@ function updateLoop() {
 
 function collisionLoop() {
 	for(var key in players) {
-		if (players.hasOwnProperty(key)) {				
-			if (players[key].alive) {				
+		for(var key2 in players) {
+			if (players.hasOwnProperty(key) && players[key].gap < 0 && players[key].alive && players.hasOwnProperty(key2) && players[key2].alive) {							
 				var x = players[key].lineData[players[key].lineData.length - 1].x,
-					y = players[key].lineData[players[key].lineData.length - 1].y;
+					y = players[key].lineData[players[key].lineData.length - 1].y
+					lastx = players[key].lineData[players[key].lineData.length - 2].x,
+					lasty = players[key].lineData[players[key].lineData.length - 2].y;
 				
 				// Collision checking
 				var i;
-				for(var key2 in players) {
-					if (players.hasOwnProperty(key2) && players[key2].alive) {
-						for (i = 0; i < players[key2].lineData.length - 2; ++i) { 
-							if (players[key2].lineData[i].y != null && players[key2].lineData[i+1].y != null) {
-								p = distToSegment({"x": x, "y": y}, players[key2].lineData[i], players[key2].lineData[i+1]);
-								//console.log("x: " + x + " y: " + y + " -  l1x: " + players[key].lineData[i].x + " l1y: " + players[key].lineData[i].y);
-								//console.log(i + ": " + players[key].lineData[i].x);
-								if (p > 0.1 && p < 1.5) {
-									players[key].alive = false;
-									io.sockets.emit('d', key);
-									console.log("COLLISION");
-								}
-							}
+				for (i = 1; i < players[key2].lineData.length - 2; ++i) { 
+					if (players[key2].lineData[i-1].y != null && players[key2].lineData[i].y != null) {
+						//p = distToSegment({"x": x, "y": y}, players[key2].lineData[i], players[key2].lineData[i+1]);
+						//console.log("x: " + x + " y: " + y + " -  l1x: " + players[key].lineData[i].x + " l1y: " + players[key].lineData[i].y);
+						//console.log(i + ": " + players[key].lineData[i].x);
+						var p = intersect(lastx, lasty, x, y, players[key2].lineData[i-1].x, players[key2].lineData[i-1].y, players[key2].lineData[i].x, players[key2].lineData[i].y);
+						if (p) {
+							players[key].alive = false;
+							io.sockets.emit('d', key);
 						}
 					}
 				}
 			}
-					
 		}
-    }
-	
-	
+	}
+				
 }
 
 function otherLoop() {
@@ -155,14 +156,78 @@ http.listen(serverPort, function() {
 
 
 // Utils
-function sqr(x) { return x * x }
-function dist2(v, w) { return sqr(v.x - w.x) + sqr(v.y - w.y) }
-function distToSegmentSquared(p, v, w) {
-  var l2 = dist2(v, w);
-  if (l2 == 0) return dist2(p, v);
-  var t = ((p.x - v.x) * (w.x - v.x) + (p.y - v.y) * (w.y - v.y)) / l2;
-  t = Math.max(0, Math.min(1, t));
-  return dist2(p, { x: v.x + t * (w.x - v.x),
-					y: v.y + t * (w.y - v.y) });
+function intersect(x1, y1, x2, y2, x3, y3, x4, y4){
+
+  var a1, a2, b1, b2, c1, c2;
+  var r1, r2 , r3, r4;
+  var denom, offset, num;
+
+  // Compute a1, b1, c1, where line joining points 1 and 2
+  // is "a1 x + b1 y + c1 = 0".
+  a1 = y2 - y1;
+  b1 = x1 - x2;
+  c1 = (x2 * y1) - (x1 * y2);
+
+  // Compute r3 and r4.
+  r3 = ((a1 * x3) + (b1 * y3) + c1);
+  r4 = ((a1 * x4) + (b1 * y4) + c1);
+
+  // Check signs of r3 and r4. If both point 3 and point 4 lie on
+  // same side of line 1, the line segments do not intersect.
+  if ((r3 != 0) && (r4 != 0) && same_sign(r3, r4)){
+    return false;
+  }
+
+  // Compute a2, b2, c2
+  a2 = y4 - y3;
+  b2 = x3 - x4;
+  c2 = (x4 * y3) - (x3 * y4);
+
+  // Compute r1 and r2
+  r1 = (a2 * x1) + (b2 * y1) + c2;
+  r2 = (a2 * x2) + (b2 * y2) + c2;
+
+  // Check signs of r1 and r2. If both point 1 and point 2 lie
+  // on same side of second line segment, the line segments do
+  // not intersect.
+  if ((r1 != 0) && (r2 != 0) && (same_sign(r1, r2))){
+    return false;
+  }
+
+  //Line segments intersect: compute intersection point.
+  denom = (a1 * b2) - (a2 * b1);
+
+  if (denom == 0) {
+    return false; // collinear
+  }
+
+  if (denom < 0){ 
+    offset = -denom / 2; 
+  } else {
+    offset = denom / 2 ;
+  }
+
+  // The denom/2 is to get rounding instead of truncating. It
+  // is added or subtracted to the numerator, depending upon the
+  // sign of the numerator.
+  num = (b1 * c2) - (b2 * c1);
+  if (num < 0){
+	x = (num - offset) / denom;
+  } else {
+    x = (num + offset) / denom;
+  }
+
+  num = (a2 * c1) - (a1 * c2);
+  if (num < 0){
+    y = ( num - offset) / denom;
+  } else {
+    y = (num + offset) / denom;
+  }
+
+  // lines_intersect
+  return true;
 }
-function distToSegment(p, v, w) { return Math.sqrt(distToSegmentSquared(p, v, w)); }
+
+function same_sign(a, b){
+  return ((a * b) >= 0);
+}
