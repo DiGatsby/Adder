867feb882d5411fb69d9ae9a9ec5639f7d8629c4
Viltzu <viltzu2@gmail.com>
2016-07-07 08:14:05 +0300

    Fixed collision detection (With slower function) and error on server when someone disconnects

--- server/server.js
+++ server/server.js
@@ -64,6 +64,10 @@ io.on('connection', function (socket) {
 		io.sockets.emit('nick', {"id": socket.id, "nick": nick});
 	});	
 	
+	socket.on('disconnect', function() {
+		delete players[socket.id];
+	});
+	
 });
 
 function updateLoop() {
@@ -128,7 +132,7 @@ function collisionLoop() {
 				
 				// Collision checking
 				var i;
-				for (i = 1; i < players[key2].lineData.length - 3; ++i) { 
+				for (i = 1; i < players[key2].lineData.length - 2; ++i) { 
 					if (players[key2].lineData[i-1].y != null && players[key2].lineData[i].y != null) {
 						//p = distToSegment({"x": x, "y": y}, players[key2].lineData[i], players[key2].lineData[i+1]);
 						//console.log("x: " + x + " y: " + y + " -  l1x: " + players[key].lineData[i].x + " l1y: " + players[key].lineData[i].y);
@@ -136,14 +140,14 @@ function collisionLoop() {
 						var p = intersect(lastx, lasty, x, y, players[key2].lineData[i-1].x, players[key2].lineData[i-1].y, players[key2].lineData[i].x, players[key2].lineData[i].y);
 						if (p) {
 							players[key].alive = false;
-							io.sockets.emit('d', key);
+							io.sockets.emit('d', {id: key, x1: lastx, y1: lasty, x2: x, y2: y, x3: players[key2].lineData[i-1].x, y3: players[key2].lineData[i-1].y, x4: players[key2].lineData[i].x, y4: players[key2].lineData[i].y});
 							break;
 						}
 					}
 				}
-				if (players[key].alive == false) {
-					break;
-				}
+			}
+			if (players[key].alive == false) {
+				break;
 			}
 		}
 		if (players[key].alive == false) {
@@ -174,78 +178,24 @@ http.listen(serverPort, function() {
 
 
 // Utils
-function intersect(x1, y1, x2, y2, x3, y3, x4, y4){
-
-  var a1, a2, b1, b2, c1, c2;
-  var r1, r2 , r3, r4;
-  var denom, offset, num;
-
-  // Compute a1, b1, c1, where line joining points 1 and 2
-  // is "a1 x + b1 y + c1 = 0".
-  a1 = y2 - y1;
-  b1 = x1 - x2;
-  c1 = (x2 * y1) - (x1 * y2);
-
-  // Compute r3 and r4.
-  r3 = ((a1 * x3) + (b1 * y3) + c1);
-  r4 = ((a1 * x4) + (b1 * y4) + c1);
-
-  // Check signs of r3 and r4. If both point 3 and point 4 lie on
-  // same side of line 1, the line segments do not intersect.
-  if ((r3 != 0) && (r4 != 0) && same_sign(r3, r4)){
+function intersect(line1StartX, line1StartY, line1EndX, line1EndY, line2StartX, line2StartY, line2EndX, line2EndY) {
+    // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
+    var denominator, a, b, numerator1, numerator2, result = {
+        x: null,
+        y: null,
+    };
+    denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
+    if (denominator == 0) { return false; }
+    a = line1StartY - line2StartY;
+    b = line1StartX - line2StartX;
+    numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
+    numerator2 = ((line1EndX - line1StartX) * a) - ((line1EndY - line1StartY) * b);
+    a = numerator1 / denominator;
+    b = numerator2 / denominator;
+    result.x = line1StartX + (a * (line1EndX - line1StartX));
+    result.y = line1StartY + (a * (line1EndY - line1StartY));
+    if (a > 0 && a < 1 && b > 0 && b < 1 && result.x != null && result.y != null) {
+        return true;
+    }
     return false;
-  }
-
-  // Compute a2, b2, c2
-  a2 = y4 - y3;
-  b2 = x3 - x4;
-  c2 = (x4 * y3) - (x3 * y4);
-
-  // Compute r1 and r2
-  r1 = (a2 * x1) + (b2 * y1) + c2;
-  r2 = (a2 * x2) + (b2 * y2) + c2;
-
-  // Check signs of r1 and r2. If both point 1 and point 2 lie
-  // on same side of second line segment, the line segments do
-  // not intersect.
-  if ((r1 != 0) && (r2 != 0) && (same_sign(r1, r2))){
-    return false;
-  }
-
-  //Line segments intersect: compute intersection point.
-  denom = (a1 * b2) - (a2 * b1);
-
-  if (denom == 0) {
-    return false; // collinear
-  }
-
-  if (denom < 0){ 
-    offset = -denom / 2; 
-  } else {
-    offset = denom / 2 ;
-  }
-
-  // The denom/2 is to get rounding instead of truncating. It
-  // is added or subtracted to the numerator, depending upon the
-  // sign of the numerator.
-  num = (b1 * c2) - (b2 * c1);
-  if (num < 0){
-	x = (num - offset) / denom;
-  } else {
-    x = (num + offset) / denom;
-  }
-
-  num = (a2 * c1) - (a1 * c2);
-  if (num < 0){
-    y = ( num - offset) / denom;
-  } else {
-    y = (num + offset) / denom;
-  }
-
-  // lines_intersect
-  return true;
-}
-
-function same_sign(a, b){
-  return ((a * b) >= 0);
-}
+};
